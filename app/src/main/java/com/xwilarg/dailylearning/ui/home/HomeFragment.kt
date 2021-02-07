8ccf40ab5b24a98e82cf4820c8fdc187a3248707
Xwilarg <xwilarg@yahoo.fr>
2021-02-07 18:14:24 +0100

    Add empty Android app

---
+++ app/src/main/java/com/xwilarg/dailylearning/ui/home/HomeFragment.kt
@@ -0,0 +1,19 @@
+package com.xwilarg.dailylearning.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.xwilarg.dailylearning.R
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val v: View = inflater.inflate(R.layout.fragment_home, container, false)
+        return v
+    }
+}
