2bf50c064da5299cb1cf6759903dc4d12106ecf2
Xwilarg <xwilarg@yahoo.fr>
2021-02-07 19:07:59 +0100

    Add Settings activity

--- app/src/main/java/com/xwilarg/dailylearning/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/xwilarg/dailylearning/ui/settings/SettingsFragment.kt
@@ -1,19 +1,39 @@
 package com.xwilarg.dailylearning.ui.settings
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
 import com.xwilarg.dailylearning.R
 
 class SettingsFragment : Fragment() {
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.preferences, rootKey)
+
+            findPreference<Preference>("github")!!.setOnPreferenceClickListener {
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Xwilarg/DailyLanguage")))
+                true
+            }
+        }
+    }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val v: View = inflater.inflate(R.layout.fragment_home, container, false)
+        val v = inflater.inflate(R.layout.fragment_settings, container, false)
+        requireActivity().supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.settings, SettingsFragment())
+            .commit()
+
         return v
     }
 }
