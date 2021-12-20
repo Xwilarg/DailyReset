2f86327b8e1d21059825efdc6d37be4dbadcf185
Xwilarg <xwilarg@yahoo.fr>
2021-12-20 18:15:08 +0100

    Fix calendar view

---
+++ app/src/main/java/com/xwilarg/dailylearning/DayViewContainer.kt
@@ -0,0 +1,9 @@
+package com.xwilarg.dailylearning
+
+import android.view.View
+import android.widget.TextView
+import com.kizitonwose.calendarview.ui.ViewContainer
+
+class DayViewContainer(view: View) : ViewContainer(view) {
+    val textView = view.findViewById<TextView>(R.id.calendarDayText)
+}
--- app/src/main/java/com/xwilarg/dailylearning/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/com/xwilarg/dailylearning/ui/calendar/CalendarFragment.kt
@@ -1,15 +1,19 @@
 package com.xwilarg.dailylearning.ui.calendar
 
-import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.kizitonwose.calendarview.CalendarView
+import com.kizitonwose.calendarview.model.CalendarDay
+import com.kizitonwose.calendarview.model.DayOwner
+import com.kizitonwose.calendarview.ui.DayBinder
 import com.xwilarg.dailylearning.*
-import kotlinx.android.synthetic.main.fragment_home.view.*
+import java.time.YearMonth
+import java.time.temporal.WeekFields
+import java.util.*
 
 class CalendarFragment : Fragment() {
 
@@ -18,6 +22,26 @@ class CalendarFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val v: View = inflater.inflate(R.layout.fragment_calendar, container, false)
+        val calendarView = v.findViewById<CalendarView>(R.id.calendarView);
+        calendarView.dayBinder = object : DayBinder<DayViewContainer> {
+            override fun create(view: View) = DayViewContainer(view)
+
+            override fun bind(container: DayViewContainer, day: CalendarDay) {
+                container.textView.text = day.date.dayOfMonth.toString()
+                if (day.owner == DayOwner.THIS_MONTH) {
+                    container.textView.setTextColor(Color.BLACK)
+                } else {
+                    container.textView.setTextColor(Color.GRAY)
+                }
+            }
+        }
+
+        val currentMonth = YearMonth.now()
+        val firstMonth = currentMonth.minusMonths(10)
+        val lastMonth = currentMonth.plusMonths(10)
+        val firstDayOfWeek = WeekFields.of(Locale.getDefault()).firstDayOfWeek
+        calendarView.setup(firstMonth, lastMonth, firstDayOfWeek)
+        calendarView.scrollToMonth(currentMonth)
         return v
     }
 }
